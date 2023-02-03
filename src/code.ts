dd95faa8989019454e9ddfc1d36ae03a222eb803
Shane Rogers <shane@repllabs.com>
2023-02-03 13:20:54 -0800

    update starting point

--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,79 @@
-type Command = "TEXT"
+// @ts-nocheck
 
-const TEXT = "Text";
+/**
+ * Figma simple parameters suggestions
+ *
+ * Note:
+ * - To simplify the code we will ignore all typescript errors by writing @ts-nocheck at the top of the file :)
+ *
+ * 1. Menu configuration
+ * - The visuals in the command palette are powered by two things:
+ * - 1. the menu options defined in the manifest
+ * - 2. the suggestions returned from the parameter 'input' event
+ * - You can configure the menu options to allow for:
+ * - - a single / multiple options
+ * - - free form input
+ * - - whether it's required
+ *
+ * 2. Responding to input / presenting suggestions
+ * - The on input event function is called everytime a user types in the command palette
+ * - We can use the command context and input query to return suggestions to the user
+ * -- i.e. if the user is typing in a font name, we can return a list of fonts that match the query
+ *
+ * 3. Running the plugin
+ * - Once all of the required menu options have been set by the user, the plugin will run
+ * - At that point the "run" event will be triggered
+ * - Within that function we have access to the command context and the set parameters
+ * - Now we can execute our plugin logic
+ * - Remember we have access to the figma global in this file so we can create text nodes etc.
+ *
+ * References:
+ * - DOCS: https://www.figma.com/plugin-docs/plugin-parameters/
+ */
+const DEFAULT_FONT_CONFIG = { family: "Inter", style: "Regular" };
 
-const COMMANDS = {
-  TEXT
+let suggestions = () => {
+  return ["A", "B", "C"];
 };
-const DEFAULT_FONT_CONFIG = { family: "Inter", style: "Regular" }
+
+// https://www.figma.com/plugin-docs/api/figma-parameters/#suggestionresults
+// - the key needs to match the parameter key from the manifest
+figma.parameters.on("input", ({ key, query, result }) => {
+  console.log("User typed: ", query);
+  switch (key) {
+    case "paramOne":
+      result.setSuggestions(suggestions());
+      break;
+    case "paramThree":
+      result.setSuggestions(suggestions());
+      break;
+    default:
+      return;
+  }
+});
 
 /**
  * Perform action after all parameters have been added
  * - DOCS: https://www.figma.com/plugin-docs/plugin-parameters/#run
  */
-figma.on("run", async ({ command, parameters }: RunEvent) => {
-  const ctxCommand = command as Command;
-
+figma.on("run", async ({ command, parameters }) => {
   if (parameters) {
-    switch (ctxCommand) {
-      case COMMANDS.TEXT:
-        await figma.loadFontAsync(DEFAULT_FONT_CONFIG);
-        const text = figma.createText()
-        text.fontName = DEFAULT_FONT_CONFIG
-        text.characters = parameters.text
-        text.fontSize = 20
+    console.log(
+      "User made these selections:",
+      JSON.stringify(
+        {
+          command,
+          parameters: parameters
+        },
+        null,
+        2
+      )
+    );
 
+    switch (command) {
+      case "commandOne":
+        break;
+      case "commandTwo":
         break;
 
       default:
